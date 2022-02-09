341d1228daf30405c24fb03a56fb0237a5333822
anisimovvb <anisimovvb@gmail.com>
2022-02-09 17:07:45 +0300

    Init, v1.0.0

---
+++ README.md
@@ -0,0 +1,330 @@
+# Rees46 React Native SDK
+
+
+## Installation
+
+Rees46 React Native SDK is available through [GitHub](https://github.com/rees46/react-native-sdk/). To install it, run next command in terminal:
+
+```
+yarn add @rees46/react-native-sdk
+```
+or 
+
+```
+yarn add https://github.com/rees46/react-native-sdk.git
+```
+
+Also need added AsyncStorage plugin:
+```
+yarn add @react-native-async-storage/async-storage
+```
+and react-native-device-info
+```
+yarn add react-native-device-info
+```
+
+For push notification:
+```
+yarn add react-native-push-notification
+yarn add @react-native-firebase/app
+yarn add @react-native-firebase/messaging
+```
+## iOS Additional Installation
+Open your `/ios/{projectName}/AppDelegate.m` file, and add the following:
+At the top of the file, import the Firebase SDK:
+```
+#import <Firebase.h>
+```
+Open a terminal window and navigate to the location of the Xcode project for your app
+```
+cd ios/
+pod install
+```
+
+## Android Additional Installation
+
+In your `android/build.gradle`
+
+```gradle
+buildscript { 
+    dependencies {
+        ...
+        //Add this \/
+        classpath 'com.google.gms:google-services:4.3.4'
+    }
+```
+In your `android/app/build.gradle` add
+
+```gradle
+apply plugin: 'com.google.gms.google-services'
+```
+
+
+# Usage
+
+SDK is used for several tasks:
+
+1. Initialize SDK and user's session
+2. Events tracking
+3. Track custom event
+4. Track push
+5. Product recommendations
+6. Product search
+7. Save profile settings
+8. Init push
+9. Set push token notification
+
+## Initialization
+
+Initialize SDK object and use it anywhere in your application. (!) Remember to initialize SDK only once on application launch.
+
+```js
+import Rees46 from '@rees46/react-native-sdk';
+
+...
+const rees46sdk = new Rees46("YOUR_SHOP_ID", "Stream")
+```
+
+## Check init
+
+```js
+rees46sdk.isInit(); // returns true/false
+```
+
+## Events tracking
+
+Track user's behavior to collect data. There are several types of events:
+
+```js
+
+
+// View product (simple way)
+rees46sdk.track("view", 37);
+
+// View product (try to avoid it)
+rees46sdk.track("view", {
+    id: 37,
+    stock: true
+});
+
+// View product after user clicked on recommender block
+rees46sdk.track("view", {
+  id: PRODUCT_ID,
+  recommended_by: 'dynamic',
+  recommended_code: 'UNIQUE_RECOMMENDER_CODE'
+});
+
+// View product, after user clicked on search results
+rees46sdk.track("view", {
+  id: PRODUCT_ID,
+  recommended_by: 'full_search',
+  recommended_code: QUERY_STRING
+});
+// ... or instant search dropdown
+rees46sdk.track("view", {
+  id: PRODUCT_ID,
+  recommended_by: 'instant_search',
+  recommended_code: QUERY_STRING
+});
+
+// View category
+rees46sdk.track("category", 100500);
+
+// Add product to cart (simple way)
+rees46sdk.track("cart", id);
+
+// Add product to cart with amount and track recommender
+rees46sdk.track("cart", {
+  id: PRODUCT_ID,
+  amount: PRODUCT_QUANTITY,
+  recommended_by: 'dynamic',
+  recommended_code: 'UNIQUE_RECOMMENDER_CODE'
+});
+
+// Remove product from cart
+rees46sdk.track("remove_from_cart", id);
+
+// Add product to favorities
+rees46sdk.track("wish", id);
+
+// Remove product from favorities
+rees46sdk.track("remove_wish", id);
+
+// Track purchase (several products)
+rees46sdk.track("purchase", {
+  products: [
+      {id: 37, price: 318, amount: 3},
+      {id: 187, price: 5000, amount: 1}
+  ],
+  order: 'N318',
+  order_price: 29999
+});
+
+// Track user search
+rees46sdk.track("search", "This is a search example");
+```
+
+## Track custom event
+```js
+// Simple tracking
+rees46sdk.trackEvent('my_event');
+
+// Tracking with custom parameters
+rees46sdk.trackEvent('my_event', {
+  category: "event category", 
+  label: "event label",
+  value: 100
+});
+```
+
+## Track push notifications
+
+```js
+const params = {
+  code: 'CODE',
+  type: 'TYPE'
+};
+// Track user click notification
+rees46sdk.notificationClicked(params);
+
+// Track Notification received
+rees46sdk.notificationReceived(params);
+
+```
+
+## Product search
+
+```js
+const type = 'instant_search'; // full_search, ...
+
+let search_query = 'your_search_text';
+
+rees46sdk.search({
+  type: type,
+  search_query: search_query,
+  // other params
+}) 
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+```
+
+## Product recommendations
+
+```js
+const recommender_code = 'recommender_code'; 
+
+const params = {
+  item: 100500,
+  exclude: [3, 14, 159, 26535],
+  search_query: "To be or not to be"
+ // other params
+};
+
+rees46sdk.recommend(recommender_code, params) 
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+```
+
+## Save profile settings
+
+```js
+const params = {
+  id: 100500,
+  email: "john.doe@examplemail.com",
+  phone: "4400114527199",
+  first_name: "John",
+  last_name: "Doe",
+  birthday: "1990-03-11",
+  age: 31,
+  gender: "m",
+  location: "NY",
+  bought_something: true,
+  loyalty_id: "000001234567",
+  loyalty_card_location: "NY",
+  loyalty_status: "5% discount",
+  loyalty_bonuses: 1123,
+  loyalty_bonuses_to_next_level: 1877,
+  fb_id: "000000000354677",
+  vk_id: "vk031845",
+  telegram_id: "0125762968357835",
+  kids: [
+    {gender: "m", birthday: "2001-04-12"},
+    {gender: "f", birthday: "2015-07-28"}
+  ],
+  auto: [
+    {brand: "Nissan", model: "Qashqai", vds: "TM7N243E4G0BJG978"}
+  ]
+};
+
+rees46sdk.setProfile(params);
+```
+
+## Init push notification
+
+```js
+// Simple init 
+rees46sdk.initPush();
+
+//onClick listener
+rees46sdk.initPush(onClickCallback);
+
+// onReceivetive listener
+rees46sdk.initPush(false, onReceiveCallback);
+
+// you can use different callback for notification, when app is in background.    
+rees46sdk.initPush(false, onReceiveCallback, onBackgroundReceiveCallback);
+// If onBackgroundReceiveCallback not specified, used onReceiveCallback listener. 
+
+// onClickCallback params
+{
+  "bigPictureUrl": "MESSAGE_IMAGE",
+  "channelId": "rees46-push", 
+  "data": {
+    "id": "MESSAGE_ID",
+    "type": "MESSAGE_TYPE"
+  }, 
+  "foreground": true, 
+  "id": "MESSAGE_ID", 
+  "largeIconUrl": "MESSAGE_ICON",
+  "message": "MESSAGE_BODY", 
+  "title": "MESSAGE_TITLE", 
+  "userInteraction": true
+}
+// onReceiveCallBack, onBackgroundReceiveCallback params
+{
+  "data": {
+    "action_urls": "[]", 
+    "actions": "[]",
+    "body": "MESSAGE_BODY",
+    "icon": "MESSAGE_ICON", 
+    "id": "MESSAGE_ID",
+    "image": "MESSAGE_IMAGE", 
+    "title": "MESSAGE_TITLE", 
+    "type": "MESSAGE_TYPE"
+  }, 
+  "from": "MESSAGE_FROM", 
+  "messageId": "FMC_MESSAGE_ID", 
+  "sentTime": TIMESTAMP, 
+  "ttl": TTL_VALUE
+}
+```
+
+## Set push token notification
+
+```js
+rees46sdk.setPushTokenNotification('NEW_TOKEN');
+```
+
+## 
+
+## License
+
+Rees46 React Native SDK is available under the MIT license. See the LICENSE file for more info.
---
+++ index.js
@@ -0,0 +1,282 @@
+import {request, updSeance} from './lib/client';
+import { convertParams } from './lib/tracker';
+import { Platform } from 'react-native';
+
+import messaging from '@react-native-firebase/messaging';
+import PushNotification from "react-native-push-notification";
+
+//Время жизни сеанса в минутах
+export const SESSION_CODE_EXPIRE = 30;
+
+class Rees46 {
+  constructor(shop_id, stream) {
+    this.channel_id = 'rees46-push';
+    this.shop_id = shop_id;
+    this.stream = stream ?? null;
+    this.initialized = false;
+    this.init();
+  }
+
+  async init() {
+    try {
+      if (!this.shop_id || typeof this.shop_id !== 'string') {
+        const initError = new Error('Parameter "shop_id" is required as a string.');
+
+        initError.name = 'Init error';
+
+        throw initError;
+      }
+
+      if (this.stream && typeof this.stream !== 'string') {
+        const streamError = new Error('Parameter "stream" must be a string.');
+
+        streamError.name = 'Init error';
+
+        throw streamError;
+      }
+
+      const response = await request('init', {
+        params: {
+          shop_id: this.shop_id,
+          stream: this.stream,
+        },
+      });
+
+      this.initialized = true;
+
+      await updSeance(response?.did, response?.seance);
+
+    } catch (error) {
+      this.initialized = false;
+      return error;
+    }
+  }
+
+  isInit = () => this.initialized;
+
+  async track(event, options) {
+    try {
+      const queryParams = await convertParams(event, options);
+      return await request('push', {
+        method: 'POST',
+        params: {
+          shop_id: this.shop_id,
+          stream: this.stream,
+          ...queryParams,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async trackEvent(event, options) {
+    try {
+      let queryParams = { event: event };
+      if (options) {
+        queryParams = Object.assign(queryParams, options);
+      }
+
+      return await request('push/custom', {
+        method: 'POST',
+        params: {
+          shop_id: this.shop_id,
+          stream: this.stream,
+          ...queryParams,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async notificationClicked(options) {
+    return await this.notificationTrack('clicked', options);
+  }
+  async notificationReceived(options) {
+    return await this.notificationTrack('received', options);
+  }
+
+  async notificationTrack(event, options) {
+    try {
+      return await request(`web_push_subscriptions/${event}`, {
+        method: 'POST',
+        params: {
+          shop_id: this.shop_id,
+          stream: this.stream,
+          ...options,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async recommend(recommender_code, options) {
+    try {
+      return await request(`recommend/${recommender_code}`, {
+        params: {
+          shop_id: this.shop_id,
+          stream: this.stream,
+          recommender_code,
+          ...options,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async search(options) {
+    try {
+      return await request('search', {
+        params: {
+          shop_id: this.shop_id,
+          stream: this.stream,
+          ...options,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async setProfile(params) {
+    if (params.hasOwnProperty("birthday") && !params.birthday.match(/^\d{4}-\d{2}-\d{2}$/)) {
+      delete params.birthday;
+    }
+    for (let key in params) {
+      if (typeof params[key] === 'object') {
+        params[key] = JSON.stringify(params[key]);
+      }
+    }
+    try {
+      return await request('profile/set', {
+        method: 'POST',
+        params: {
+          shop_id: this.shop_id,
+          stream: this.stream,
+          ...params
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+  async getProfile(options) {
+    try {
+      return await request('profile', {
+        method: 'GET',
+        params: {
+          shop_id: this.shop_id,
+          stream: this.stream,
+          ...options
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async setPushTokenNotification(token) {
+    try {
+      const params = {
+        token: token,
+        platform: Platform.OS === 'ios' ? 'ios_firebase' : 'android',
+      }
+      return await request('mobile_push_tokens', {
+        method: 'POST',
+        params: {
+          shop_id: this.shop_id,
+          stream: this.stream,
+          ...params
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+  async initPush(notifyClick, notifyReceive, notifyBgReceive)
+  {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      messaging()
+        .getToken()
+        .then(token => {
+          this.setPushTokenNotification(token);
+        });
+
+      // Register handler
+      messaging().onMessage(async remoteMessage => {
+        if (!notifyReceive) {
+          await this.showNotification(remoteMessage);
+        } else{
+          notifyReceive(remoteMessage);
+        }
+      });
+
+      // Register background handler
+      messaging().setBackgroundMessageHandler(async remoteMessage => {
+        if (!notifyReceive && !notifyBgReceive) {
+          await this.showNotification(remoteMessage);
+        } else if (!notifyBgReceive) {
+          notifyReceive(remoteMessage);
+        } else {
+          notifyBgReceive(remoteMessage);
+        }
+      });
+
+      //Subscribe to click  notification
+      PushNotification.configure({
+        onNotification: (notification) => {
+          if (notification?.userInteraction === true) {
+            if (!notifyClick) {
+              this.notificationClicked({
+                code: notification?.data?.id,
+                type: notification?.data?.type
+              });
+            } else {
+              notifyClick(notification);
+            };
+          };
+        }
+      });
+
+      PushNotification.channelExists(this.channel_id, function (exists) {
+        if (!exists) {
+          PushNotification.createChannel(
+            {
+              channelId: 'rees46-push',
+              channelName: 'rees46 channel',
+            }
+          );
+        }
+      });
+    }
+  }
+  async showNotification (message){
+    await this.notificationReceived({
+      code: message.data.id,
+      type: message.data.type
+    });
+
+    let localData = {
+      channelId: this.channel_id,
+      largeIconUrl: message.data?.icon,
+      bigPictureUrl: message.data?.image,
+      title: message.data?.title,
+      message: message.data?.body,
+      userInfo: {
+        message_id: message.data?.id,
+        type: message.data.type
+      }
+    }
+    return PushNotification.localNotification(localData);
+  };
+}
+
+export default Rees46;
