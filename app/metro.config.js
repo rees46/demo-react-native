48788fa9b393207266e55d62ee3f3cfb8954b525
Nikita Elfimov <nelfimov@mail.ru>
2024-02-20 09:03:59 +0300

    feat: connect sdk to dev app

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,42 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [path.resolve(path.join(__dirname, '..'))],
+  transformer: {
+    getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: true,
+          inlineRequires: true,
+        },
+      }
+    ),
+  },
+  resolver: {
+    unstable_enableSymlinks: true,
+    nodeModulesPaths: [path.resolve(path.join(__dirname, './node_modules'))],
+    resolveRequest: (context, moduleName, platform) => {
+      if (
+        // Add to this list whenever a new React-reliant dependency is added
+        moduleName.startsWith('react') ||
+        moduleName.startsWith('@react-native') ||
+        moduleName.startsWith('@react-native-community') ||
+        moduleName.startsWith('@your-org')
+      ) {
+        const pathToResolve = path.resolve(
+          __dirname,
+          'node_modules',
+          moduleName
+        );
+        return context.resolveRequest(context, pathToResolve, platform);
+      }
+      // Optionally, chain to the standard Metro resolver.
+      return context.resolveRequest(context, moduleName, platform);
+    },
+  },
+  nodeModulesPaths: [
+    path.resolve(path.join(__dirname, './node_modules')),
+  ],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
